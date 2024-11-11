8b1f51719fc78943c84d202310f666c7cb87f672
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-11-11 12:41:41 -0600

    linked the app module correctly

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+import styles from 'Portfolio/src/App.module.css'
 
 function App() {
-  
-
-  return (
-    <>HELLO WORLD</>
-  )
+  return <div>HELLO WORLD!!!!</div>;
 }
 
 export default App
--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -8,6 +8,6 @@ import "@fontsource/outfit"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
