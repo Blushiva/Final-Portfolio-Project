abc81fd62602dd3002b651e837675f417a51faea
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-11-11 16:11:30 -0600

    added a bunch of things to prep for later

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
-import styles from 'Portfolio/src/App.module.css';
+
+import styles from './App.module.css';
 
 function App() {
   return <div className= {styles.App}>HELLO WORLD!!!!</div>;
--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'Portfolio/src/index.css';
-import App from 'Portfolio/src/App.jsx';
+import './index.css';
+import App from './App.jsx';
 
 import "@fontsource/roboto"
 import "@fontsource/outfit"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
