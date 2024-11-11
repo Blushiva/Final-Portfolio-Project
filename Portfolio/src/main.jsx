f5d4a5548a91328c8824d2681406806c755a4647
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-11-11 15:01:26 -0600

    added folders for later

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'Portfolio/src/index.css';
+import App from 'Portfolio/src/App.jsx';
 
 import "@fontsource/roboto"
 import "@fontsource/outfit"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
